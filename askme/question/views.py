f72d2dfaf5cb78a0ca1b86826e8a49725da7da9c
Vlad104 <vlad104da@gmail.com>
2018-10-23 01:16:17 +0300

    dz 1 2

--- askme/question/views.py
+++ askme/question/views.py
@@ -6,8 +6,14 @@ from django.shortcuts import render
 # Create your views here. ## MY code
 def index(request):
 	questions_list = [
-		{"name" : "my first question", "id": 1}, 
-		{"name" : "my second question", "id": 2},
+		{"name" : "Как", "id": 1}, 
+		{"name" : "перестать", "id": 2},
+		{"name" : "делать", "id": 3}, 
+		{"name" : "сайты???", "id": 4},
+		{"name" : "my fifth question", "id": 5}, 
+		{"name" : "my sixth question", "id": 6},
+		{"name" : "my seventh question", "id": 7}, 
+		{"name" : "my eigth question", "id": 8},
 	]
 	return render(request, "question/index.html", {
 		"questions": questions_list,
