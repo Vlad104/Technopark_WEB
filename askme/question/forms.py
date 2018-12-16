21ca73e66a5b63791d466440954f35e393c7151f
Vlad104 <vlad104da@gmail.com>
2018-12-16 23:32:09 +0300

    dz4

--- askme/question/forms.py
+++ askme/question/forms.py
@@ -1,3 +1,151 @@
 from django import forms
-
+from django.core.validators import RegexValidator
+from django.core.exceptions import ValidationError
+from django.contrib.auth import authenticate
 from question.models import Question, Tag, User, Answer, Like
+
+text_validator = RegexValidator(r"[а-яА-Яa-zA-Z]", "Текст должен содержать буквы")
+tags_validator = RegexValidator(r"[а-яА-Яa-zA-Z]", "Tags should contain letters")
+password_validator = RegexValidator(r"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$",
+                                   "Password should contain minimum 8 characters, at least 1 letter and 1 number")
+
+class UserRegistrationForm(forms.ModelForm):
+    first_name = forms.CharField(validators=[text_validator], widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'minlength': 2,
+        'maxlength': 30,
+        'placeholder': 'First name'
+    }))
+
+    last_name = forms.CharField(validators=[text_validator],widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'minlength': 2,
+        'maxlength': 30,
+        'placeholder': 'Last name'
+    }))
+
+    username = forms.CharField(validators=[text_validator],widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'minlength': 5,
+        'maxlength': 30,
+        'placeholder': 'Username'}))
+
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'E-mail'}))
+
+    password = forms.CharField(validators=[password_validator], widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Password'}))
+
+    password_confirmation = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Password confirmation'}))
+
+    def clean(self):
+        cleaned_data = super(UserRegistrationForm, self).clean()
+        password = cleaned_data.get("password")
+        password_confirmation = cleaned_data.get("password_confirmation")
+
+        if password != password_confirmation:
+            raise ValidationError("Password and Confirm password does not match")
+        return cleaned_data
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'upload']
+
+
+
+class UserLoginForm(forms.ModelForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'maxlength': 30,
+        'placeholder': 'Username'}))
+
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Password'}))
+
+    def clean(self):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
+        if not user or not user.is_active:
+            raise ValidationError("Sorry, that login or password is invalid. Please try again.")
+        return self.cleaned_data
+
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+
+
+class UserSettingsForm(forms.ModelForm):
+    first_name = forms.CharField(required=False,validators=[text_validator],widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'minlength': 2,
+        'maxlength': 30,
+        'placeholder': 'First name'}))
+
+    last_name = forms.CharField(required=False,validators=[text_validator], widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'minlength': 2,
+        'maxlength': 30,
+        'placeholder': 'Last name'}))
+
+    username = forms.CharField(validators=[text_validator],required=False,widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'minlength': 5,
+        'maxlength': 30,
+        'placeholder': 'Username'}))
+
+    email = forms.EmailField(required=False,widget=forms.EmailInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'E-mail'}))
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'upload']
+
+
+class AskForm(forms.ModelForm):
+    title = forms.CharField(validators=[text_validator],widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'maxlength': 100,
+        'minlength': 5,
+        'placeholder': 'Title'}))
+
+    text = forms.CharField(validators=[text_validator],widget=forms.Textarea(attrs={
+        'class': 'form-control',
+        'minlength': 5,
+        'placeholder': 'Write your question here...'}))
+
+    tags = forms.CharField(required=False, validators=[text_validator],widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'List here tags by separating them with a space.'}))
+
+    def clean(self):
+        title = self.cleaned_data.get('title')
+        tags = self.cleaned_data.get('tags')
+        if len(str(title)) > 100:
+            raise ValidationError("Sorry, a title should contain no more than 100 characters. ")
+        if len(str(tags)) > 20:
+            raise ValidationError("Sorry, length of tags line must be no more than 20 characters.  ")
+        return self.cleaned_data
+
+    class Meta:
+        model = Question
+        fields = ['title', 'text', 'tags']
+
+
+
+class AnswerForm(forms.ModelForm):
+    text = forms.CharField(widget=forms.Textarea(attrs={
+        'class': 'form-control',
+        'minlength': 1,
+        'placeholder': 'Write your answer here...'}))
+
+    class Meta:
+        model = Answer
+        fields = ['text']
--- askme/question/views.py
+++ askme/question/views.py
@@ -1,18 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
-from django.views.generic import ListView
+#from django.views.generic import ListView
+from django.contrib.auth import authenticate, login, logout
+from django.http import Http404, HttpResponseForbidden as Http403, HttpResponse
+from django.contrib.auth.decorators import login_required
+import json
 from question.models import Question, Tag, User, Answer, Like
-
-# Create your views here. ## MY code
+from question.forms import UserRegistrationForm, UserLoginForm, UserSettingsForm, AskForm, AnswerForm
 
 def index(request):
     return render(request, 'question/index.html', {
 	    	'questions': paginate(request, Question.objects.all()),
             'tags' : paginate(request, Tag.objects.hottest()),
             'users' : paginate(request, User.objects.by_rating()),
+            'page_objects' : paginate(request, Question.objects.all()),
     	})
 
 def top(request):
@@ -20,6 +24,7 @@ def top(request):
             'questions': paginate(request, Question.objects.get_hot()),
             'tags' : paginate(request, Tag.objects.hottest()),
             'users' : paginate(request, User.objects.by_rating()),
+            'page_objects' : paginate(request, Question.objects.all()),
         })
 
 def new(request):
@@ -27,6 +32,7 @@ def new(request):
             'questions': paginate(request, Question.objects.get_new()),
             'tags' : paginate(request, Tag.objects.hottest()),
             'users' : paginate(request, User.objects.by_rating()),
+            'page_objects' : paginate(request, Question.objects.all()),
         })
 
 def user(request, id):
@@ -41,6 +47,7 @@ def user_questions(request, id):
             'questions': paginate(request, Question.objects.get_by_user(user_id=id)),
             'tags' : paginate(request, Tag.objects.hottest()),
             'users' : paginate(request, User.objects.by_rating()),
+            'page_objects' : paginate(request, Question.objects.get_by_user(user_id=id)),
     })    
 
 def tag(request, id):
@@ -56,26 +63,98 @@ def question(request, id):
             'answers' : paginate(request, Answer.objects.get_hot_for_answer(id)),
             'tags' : paginate(request, Tag.objects.hottest()),
             'users' : paginate(request, User.objects.by_rating()),
+            'page_objects' : paginate(request, Answer.objects.get_hot_for_answer(id)),
    	})
 
+
+#@login_required(login_url='/signin/')
+def new_answer(request, question_id):
+    if Question.objects.filter(id=question_id).exists():
+        if request.method == 'POST':
+            form = AnswerForm(request.POST)
+            if form.is_valid():
+                answeredQuestion = Question.objects.get_by_id(question_id)[0]
+                answer = Answer.objects.create(author=request.user,
+                                date=timezone.now(),
+                                text=form.cleaned_data['text'],
+                                question_id=answeredQuestion.id)
+                answer.save()
+                return redirect('/question/{}/#{}'.format(question_id, answer.id))
+        else:
+            form = AnswerForm()
+        return render(request, 'question/new_answer.html', {'form': form})
+    else:
+        raise Http404
+
+@login_required(login_url='/login/')
 def ask(request):
-	return render(request, "question/ask.html", {
+    if request.method == 'POST':
+        form = AskForm(request.POST)
+        if form.is_valid():
+            ques = Question.objects.create(author=request.user,
+                            date=timezone.now(),
+                            is_active=True,
+                            title=form.cleaned_data['title'],
+                            text=form.cleaned_data['text'])
+            ques.save()
+
+            for tagTitle in form.cleaned_data['tags'].split():
+                tag = Tag.objects.get_or_create(name=tagTitle)[0]
+                ques.tags.add(tag)
+                ques.save()
+            return question(request, ques.id)
+    else:
+        form = AskForm()
+    return render(request, 'question/ask.html', {
+            'form': form,
             'tags' : paginate(request, Tag.objects.hottest()),
             'users' : paginate(request, User.objects.by_rating()),
-    })
+        })
 
 
-def login(request):
-    return render(request, "question/login.html", {
+def signin(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect(request.GET.get('next') if request.GET.get('next') != '' else '/')
+    else:
+        form = UserLoginForm()
+        logout(request)
+    return render(request, 'question/login.html', {
+            'form': form,
             'tags' : paginate(request, Tag.objects.hottest()),
             'users' : paginate(request, User.objects.by_rating()),
-    })
+        })
 
 def registration(request):
-    return render(request, "question/registration.html", {
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save()
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = UserRegistrationForm()
+        logout(request)
+    return render(request, 'question/registration.html', {
+            'form': form,
             'tags' : paginate(request, Tag.objects.hottest()),
             'users' : paginate(request, User.objects.by_rating()),
-    })
+        })
+
+def signout(request):
+    if not request.user.is_authenticated:
+        raise Http404
+    logout(request)
+    return redirect(request.GET['from'])
+
 
 def paginate(request, objects_list):
     paginator = Paginator(objects_list, 5)
